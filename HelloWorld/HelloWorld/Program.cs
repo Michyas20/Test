a5a965ab121cac6e1d753dda250c6f7fe3f24a62
Michyas20 <101133166+Michyas20@users.noreply.github.com>
2022-06-18 16:53:14 +0300

    Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Git
+{
+    public class Project
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
