d845b3d19a22bbd4e2b83d87ddb0a22bfddd2e8d
Michyas20 <101133166+Michyas20@users.noreply.github.com>
2022-06-29 22:43:18 +0300

    while loop and if statements

---
+++ While loops and if statments/While loops and if statments/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace project
+{
+    public class guessGame
+    {
+        public static void Main(string[] args)
+        {
+            string secretWord = "pop";
+            string guess = "";
+            int attempts = 0;
+            int allowed = 3;
+            bool outOfGuesses = false;
+
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (attempts < allowed)
+                {
+                    Console.Write("Guess the word : ");
+                    guess = Console.ReadLine();
+                    attempts++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You loose!!");
+
+            }
+            else
+            {
+                Console.WriteLine("You guessed right");
+            }
+
+        }
+
+    }
+
+}
